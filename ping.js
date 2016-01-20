b736820f254f83c3e66655af71f7dc2ffb5efe0a
Francis <dev@frpz.fr>
2016-01-20 15:49:30 +0100

    change rounding to first decimal

--- ping.js
+++ ping.js
@@ -40,7 +40,7 @@ ping = function(address, nb, log, callback) {
 			if(platform == "windows")
 				latency = parseInt(body.match(/\d+ms/g).pop());
 			else
-				latency = Math.round(parseFloat(body.match(/(rtt|round-trip).*/g).pop().match(/[^\/=]+/g)[5]));
+				latency = Math.round(parseFloat(body.match(/(rtt|round-trip).*/g).pop().match(/[^\/=]+/g)[5])*10)/10;
 			lost       = parseInt(/(\d+)%/.exec(body).pop());
 			status     = ! lost > 0;
 		}catch(e){ } //no need to do anything because of default values
